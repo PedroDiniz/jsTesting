5c27dd8263788ba038c15b22e72005ab119f2c3b
PedroDiniz <pedro_nork@hotmail.com>
2022-10-17 13:11:37 -0300

    finishing coverage study

--- 04-coverage-e2e/api.js
+++ 04-coverage-e2e/api.js
@@ -1,27 +1,46 @@
-const http = require('http')
+const http = require("http");
+const DEFAULT_USER = { userName: "Peter", password: "strongPass" };
 
 const routes = {
-  '/contact:get': (req, res) => {
-    res.write('contact us page')
-    return res.end()
+  "/contact:get": (req, res) => {
+    res.write("contact us page");
+    return res.end();
+  },
+  "/login:post": async (request, response) => {
+    // response é um iterator!
+    for await (const data of request) {
+      const user = JSON.parse(data);
+      if (
+        user.username !== DEFAULT_USER.username ||
+        user.password !== DEFAULT_USER.password
+      ) {
+        response.writeHead(401);
+        response.write("Logging failed!");
+        return response.end();
+      }
+
+      response.write("Logging has succeeded!");
+      return response.end();
+    }
   },
   default: (req, res) => {
-    res.write('hello')
-    return res.end()
-  }
-}
+    res.write("hello");
+    return res.end();
+  },
+};
 
 const handler = function (req, res) {
-  const { url, method } = req
-  const routeKey = `${url}:${method.toLowerCase()}`
-  const chosenRoute = routes[routeKey] || routes.default
+  const { url, method } = req;
+  const routeKey = `${url}:${method.toLowerCase()}`;
+  const chosenRoute = routes[routeKey] || routes.default;
   res.writeHead(200, {
-     'Content-Type': 'text/html' 
-  })
-  return chosenRoute(req, res)
-}
+    "Content-Type": "text/html",
+  });
+  return chosenRoute(req, res);
+};
 
-const app = http.createServer(handler)
-                .listen(3000, () => console.log('Listening on port: ', 3000))
+const app = http
+  .createServer(handler)
+  .listen(3000, () => console.log("Listening on port: ", 3000));
 
-module.exports = app
+module.exports = app;
--- 04-coverage-e2e/api.test.js
+++ 04-coverage-e2e/api.test.js
@@ -1,23 +1,38 @@
-const { describe, it } = require('mocha')
-const request = require('supertest')
-const app = require('./api')
-const assert = require('assert')
+const { describe, it } = require("mocha");
+const request = require("supertest");
+const app = require("./api");
+const assert = require("assert");
 
-describe('API Suite test', () => {
-  describe('/contact', () => {
-    it('should return contact us page and 200 HTTP status code', async() => {
-        const res = await request(app)
-                  .get('/contact')
-                  .expect(200)  
-       assert.deepStrictEqual(res.text, 'contact us page')
-    })
-  })
-  describe('/404', () => {
-    it('should request inexistent route /hi and redirect to /hello', async() => {
-        const res = await request(app)
-                  .get('/hi')
-                  .expect(200)  
-       assert.deepStrictEqual(res.text, 'hello')
-    })
-  })
-})
+describe("API Suite test", () => {
+  describe("/contact", () => {
+    it("should return contact us page and 200 HTTP status code", async () => {
+      const res = await request(app).get("/contact").expect(200);
+      assert.deepStrictEqual(res.text, "contact us page");
+    });
+  });
+  describe("/404", () => {
+    it("should request inexistent route /hi and redirect to /hello", async () => {
+      const res = await request(app).get("/hi").expect(200);
+      assert.deepStrictEqual(res.text, "hello");
+    });
+  });
+
+  describe("/login", () => {
+    it("should login successfuly on the login route and return HTTP status 200", async () => {
+      const res = await request(app)
+        .post("/login")
+        .send({ userName: "Peter", password: "strongPass" })
+        .expect(200);
+      assert.deepStrictEqual(res.text, "Logging has succeeded!");
+    });
+
+    it("should unauthorize a request when requesting it using wrong credentials and retur HTTP status 401", async () => {
+      const res = await request(app)
+        .post("/login")
+        .send({ userName: "Peter0", password: "strongPass0" })
+        .expect(401);
+      assert.ok(res.unauthorized);
+      assert.deepStrictEqual(res.text, "Logging failed!");
+    });
+  });
+});
